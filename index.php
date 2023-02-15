f7028fd83943fa60448251eaf89bb5d6d7bf281b
Mageuss <mateus.santos@aedb.br>
2023-02-15 20:54:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+//Crie essas variáveis
+
+$numeroEmString = "2";
+$numeroEmInt = 2;
+
+// if (gettype($numeroEmInt) == gettype($numeroEmString)) {
+//     return;
+// } else {
+    
+// };
+
+// echo "a variavel $numeroEmInt é um ".gettype($numeroEmInt).".";
+// echo "<br> a variavel $numeroEmString é um ".gettype($numeroEmString).".";
+
+// settype($numeroEmString, gettype($numeroEmInt));
+
+
+// echo "<br> <br> a variavel $numeroEmInt é um ".gettype($numeroEmInt).".";
+// echo "<br> a variavel $numeroEmString é um ".gettype($numeroEmString).".";
+
+//e escreva um algoritmo para definir se seus respectivos conteúdos são iguais.
+
+
+//Crie essas variáveis
+
+// $numeroEmString = "2";
+// $numeroEmInt = 2;
+
+// if (gettype($numeroEmInt) == gettype($numeroEmString)) {
+
+//     echo "essa variaveis são do mesmo tipo. :D";
+
+// } else {
+
+//     echo "essa variaveis não são do mesmo tipo. D:";    
+
+// };
+
+//e imprima uma mensagem caso as variáveis sejam do mesmo tipo.
+
+
+
+?>
