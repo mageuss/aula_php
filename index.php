ead3615f7e80b38d5849caea9c8fb83b32f0bbdf
mageeus <mateusk747@gmail.com>
2023-03-08 21:17:15 -0300

    mexendo com get/post, funcao e redirecionamento

--- index.php
+++ index.php
@@ -1,45 +1,39 @@
-<?php
+<html>
 
-//Crie essas variáveis
+<body>
+    <?php
 
-$numeroEmString = "2";
-$numeroEmInt = 2;
+    echo "hello world :D";
 
-// if (gettype($numeroEmInt) == gettype($numeroEmString)) {
-//     return;
-// } else {
-    
-// };
+    if(isset($_GET['erro']))
+    {
+        $mess = "</br> vc teve um erro de ".$GET['erro'];
+    }
 
-// echo "a variavel $numeroEmInt é um ".gettype($numeroEmInt).".";
-// echo "<br> a variavel $numeroEmString é um ".gettype($numeroEmString).".";
+        ?>
 
-// settype($numeroEmString, gettype($numeroEmInt));
+    <form action="recebe_login.php" method='POST'>
 
+        <label for="user_nome"></lable>
 
-// echo "<br> <br> a variavel $numeroEmInt é um ".gettype($numeroEmInt).".";
-// echo "<br> a variavel $numeroEmString é um ".gettype($numeroEmString).".";
+            Nome:</br>
+            <input type="text" name="user_nome">
+            <label for="user_senha"></label></br>
 
-//e escreva um algoritmo para definir se seus respectivos conteúdos são iguais.
-
-
-//Crie essas variáveis
-
-// $numeroEmString = "2";
-// $numeroEmInt = 2;
-
-// if (gettype($numeroEmInt) == gettype($numeroEmString)) {
-
-//     echo "essa variaveis são do mesmo tipo. :D";
-
-// } else {
-
-//     echo "essa variaveis não são do mesmo tipo. D:";    
-
-// };
-
-//e imprima uma mensagem caso as variáveis sejam do mesmo tipo.
+            Senha:</br>
+            <input type="password" name="user_senha"></br>
+            <input type="submit" value="enviar">
 
 
+    </form>
 
-?>
+    <?php
+        if (isset($mess))
+        {
+            echo $mess;
+        }
+    ?>
+
+</body>
+
+</html>
