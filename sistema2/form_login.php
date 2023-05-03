caa408d71b3e1e76916b08bb9d5c5f6b1ef7dcc0
mageuss <mateus.santos@aedb.br>
2023-05-03 19:50:49 -0300

    upei o sistema2

---
+++ sistema2/form_login.php
@@ -0,0 +1,37 @@
+<?php
+require_once("funcao.php");
+require_once("header.php");
+revalidarLogin();
+?>
+
+<body>
+
+    <?php require_once("menu.php") ?>
+
+    <div class="content">
+        <table>
+
+            <tr>
+                <td>dslogin</td>
+                <td>dssenha</td>
+                <td>idaluno</td>
+                <td>nmaluno</td>
+            </tr>
+            <?php
+            
+
+                /*
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+            </tr>
+            */
+            ?>
+        </table>
+    </div>
+
+</body>
+
+</html>
