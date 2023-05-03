755ab579360cbac328135312bf4e94a20444b321
mageuss <mateus.santos@aedb.br>
2023-05-03 19:51:00 -0300

    upei sistema2

---
+++ sistema2/test.php
@@ -0,0 +1,4 @@
+<?php
+require_once("funcao.php");
+echo "Aqui";
+revalidarLogin();
---
+++ sistema2/welcome.php
@@ -0,0 +1,18 @@
+<?php
+require_once("funcao.php");
+require_once("header.php");
+revalidarLogin();
+?>
+
+<body>
+
+    <?php require_once("menu.php") ?>
+
+    <div class="content">
+        <h2>Página inicial. Bem vindo!</h2>
+        <p>Escolha uma opção no menu a esquerda</p>
+    </div>
+
+</body>
+
+</html>
